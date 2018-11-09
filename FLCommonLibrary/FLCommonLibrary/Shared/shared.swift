da90c93c8abefbc846e6e5860ba457d56c4d828e
playground101 <playgrounddfw@gmail.com>
2018-11-09 14:38:15 -0600

    create project

---
+++ FLCommonLibrary/FLCommonLibrary/Shared/shared.swift
@@ -0,0 +1,9 @@
+//
+//  shared.swift
+//  FLCommonLibrary
+//
+//  Created by Mekhala Vithala on 11/9/18.
+//  Copyright © 2018 playground. All rights reserved.
+//
+
+import Foundation
---
+++ FLCommonLibrary/FLCommonLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLCommonLibrary
+//
+//  Created by Mekhala Vithala on 11/9/18.
+//  Copyright © 2018 playground. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
